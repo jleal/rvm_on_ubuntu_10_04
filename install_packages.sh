5273de4b942b69b442c319029890d989daf19baf
Juan Leal <sellingangle@hotmail.com>
2010-09-09 05:39:24 -0500

    first commit

---
+++ install_packages.sh
@@ -0,0 +1,9 @@
+sudo apt-get install build-essential
+sudo apt-get install libc6 libpcre3 libpcre3-dev libpcrecpp0 libssl0.9.8 libssl-dev zlib1g zlib1g-dev lsb-base
+sudo apt-get install curl patch
+sudo apt-get install libopenssl-ruby libopenssl-ruby1.9 libssl-dev openssl
+sudo apt-get install libxslt-dev libxml2-dev
+sudo apt-get install vim
+sudo apt-get install git-core
+sudo apt-get install network-manager-openvpn
+sudo apt-get install postgresql postgresql-client libpq-dev psqladmin3
